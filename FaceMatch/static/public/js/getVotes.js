8ec4949ced4aa8be70ee5712bdf09ffa2086f367
Uberi <azhang9@gmail.com>
2015-03-28 12:46:18 -0400

    Stuff.

--- FaceMatch/static/public/js/getVotes.js
+++ FaceMatch/static/public/js/getVotes.js
@@ -1,46 +1,39 @@
-  function yesVotes(id)
-{
-    id.innerText= "1234 Votes";
-    var noButton = document.getElementById("noButton");
-    noButton.innerText="33 Votes";
-    console.log(id);
-
-    //var heart = document.getElementById("heart");
-    $("#heart").animate({
-    	marginTop: "60px"
-    }, 800,"easeOutBounce", function(){
-
-    });
-
-    id.disabled=true;
-    noButton.disabled=true;
-
-    httpGet();
-
+function getMatches(yes_or_no) {
+    
 }
 
-function noVotes(id)
-{
-    id.innerText= "453 Votes";
-    var yesButton = document.getElementById("yesButton");
-    yesButton.innerText="753 Votes";
-    console.log("Yes votes" + id);
+function yesVotes(id) {
+  var result = getMatches(0);
+  $("#noButton").text(result[0] + " votes");
+  $("#yesButton").text(result[1] + " votes");
+  console.log(id);
 
-    //var crying = document.getElementById("crying");
-     $("#crying").animate({
-    	marginTop: "60px"
-    }, 800, "easeOutBounce", function(){
+  $("#heart").animate({
+    marginTop: "60px"
+  }, 800,"easeOutBounce");
 
-    });
-    id.disabled=true;
-    yesButton.disabled=true;
+  id.disabled=true;
+  noButton.disabled=true;
+
+  updatePair();
 }
 
-function httpGet() {
+function noVotes(id) {
+  var result = getMatches(0);
+  id.innerText =  + " votes";
+  var yesButton = document.getElementById("yesButton");
+  yesButton.innerText="753 Votes";
+  console.log("Yes votes" + id);
 
-	var url = 'http://localhost:5000/pairs';
-	$.getJSON(url, function (data) {
-		$('#right-image').attr('src', data.right);
-	});
+  //var crying = document.getElementById("crying");
+  $("#crying").animate({marginTop: "60px"}, 800, "easeOutBounce");
+  id.disabled=true;
+  yesButton.disabled=true;
+}
+
+function updatePair() {
+  var url = 'http://localhost:5000/pairs';
+  $.getJSON(url, function (data) { $('#left-image').attr('src', data.left); });
+  $.getJSON(url, function (data) { $('#right-image').attr('src', data.right); });
 }
 
