b9aa12cd99e1e3393d8204e59e57272f32ed169f
c9dong <davidCdong_123@hotmail.com>
2015-03-28 15:01:34 -0400

    win/lose

--- FaceMatch/static/public/js/getVotes.js
+++ FaceMatch/static/public/js/getVotes.js
@@ -1,21 +1,32 @@
+var totalWin = 0;
 function updateMatches(yes_or_no) {
   $.getJSON("/matches?left=" + encodeURIComponent($('#left-image').attr("src")) +
                     "&right=" + encodeURIComponent($('#right-image').attr("src")) +
                     "&yes_or_no=" + yes_or_no, function (data) {
     $("#noButton").text(data.chose_no + " ❤").prop("disabled", true);
     $("#yesButton").text(data.chose_yes + " ❤").prop("disabled", true);
+
+    if((data.chose_yes >= data.chose_no && yes_or_no == 1) || (data.chose_no >= data.chose_yes && yes_or_no == 0)){
+      //Win
+      totalWin ++;
+      if(yes_or_no == 1){
+        $("#heart").animate({marginTop: "60px"}, 800, "easeOutBounce", function() {transitionToNewMatch();});
+      }else{
+        $("#crying").animate({marginTop: "60px"}, 800, "easeOutBounce", function() {transitionToNewMatch();});
+      }
+    }else{
+      //Lose
+      
+    }
   });
 }
 
 function yesVotes() {
   updateMatches(1);
-  $("#heart").animate({marginTop: "60px"}, 800, "easeOutBounce", function() {transitionToNewMatch();
-});
 }
 
 function noVotes() {
   updateMatches(0);
-  $("#crying").animate({marginTop: "60px"}, 800, "easeOutBounce", function() {transitionToNewMatch();});
 }
 
 function updatePair() {
