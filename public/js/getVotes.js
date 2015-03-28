0a59f3efde4b5034df550fda3518013147f9259e
Sally-Yang-Jing-Ou <sally_auyoung@hotmail.com>
2015-03-28 05:14:04 -0400

    more changes

---
+++ public/js/getVotes.js
@@ -0,0 +1,14 @@
+  function yesVotes(id)
+{
+    id.innerText= "1234 Votes";
+    document.getElementById("noButton").innerText="33 Votes";
+    console.log("Yes votes" + id);
+}
+
+function noVotes(id)
+{
+    id.innerText= "453 Votes";
+    document.getElementById("yesButton").innerText="753 Votes";
+    console.log("Yes votes" + id);
+}
+
