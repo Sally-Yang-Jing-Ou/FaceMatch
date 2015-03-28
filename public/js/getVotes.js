bc4376dbd72232cd50c32a31f502c4ff5d61ca5b
c9dong <davidCdong_123@hotmail.com>
2015-03-28 09:43:14 -0400

    heart

--- public/js/getVotes.js
+++ public/js/getVotes.js
@@ -3,6 +3,9 @@
     id.innerText= "1234 Votes";
     document.getElementById("noButton").innerText="33 Votes";
     console.log("Yes votes" + id);
+
+    var heart = document.getElementById("heart");
+    heart.style.opacity = 1;
 }
 
 function noVotes(id)
