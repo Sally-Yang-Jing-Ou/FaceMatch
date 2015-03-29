989ce9adb4da0b1cf0c45f06738b792b0ff04fb2
Sally-Yang-Jing-Ou <sally_auyoung@hotmail.com>
2015-03-28 21:10:31 -0400

    change

--- static/public/js/getVotes.js
+++ static/public/js/getVotes.js
@@ -3,8 +3,8 @@ function updateMatches(yes_or_no) {
   $.getJSON("/matches?left=" + encodeURIComponent($('#left-image').attr("src")) +
                     "&right=" + encodeURIComponent($('#right-image').attr("src")) +
                     "&yes_or_no=" + yes_or_no, function (data) {
-    $("#noButton").text(data.chose_no + " ❤").prop("disabled", true);
-    $("#yesButton").text(data.chose_yes + " ❤").prop("disabled", true);
+    $("#noButton").text(data.chose_no + " Votes").prop("disabled", true);
+    $("#yesButton").text(data.chose_yes + " Votes").prop("disabled", true);
 
     if((data.chose_yes >= data.chose_no && yes_or_no == 1) || (data.chose_no >= data.chose_yes && yes_or_no == 0)){
       //Win
--- static/public/js/startGame.js
+++ static/public/js/startGame.js
@@ -2,7 +2,6 @@ function startGame() {
     $("#start-game").animate({
       top: '-150%',
       }, 1000, function(){
-      complete();
     })
 
     $('#start-game').css('display: none');
