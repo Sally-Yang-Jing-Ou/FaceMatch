6a53bf65dd3e80802cbc477bbeba9ce23f793815
Jordan Verasamy <j.verasamy@gmail.com>
2015-03-28 21:49:54 -0400

    updated custom reddit/imgur downloader tool

--- imgurscraper.py
+++ imgurscraper.py
@@ -6,14 +6,14 @@ import time
 import os
 import sys
 
-subreddit_name = "kpics"
-gender = "F"
+subreddit_name = "kpecs"
+gender = "M"
 
-r = praw.Reddit('Experimenting with praw')
+r = praw.Reddit('scraping data for use with facematch')
 subreddit = r.get_subreddit(subreddit_name)
+amount = 1000;
 
 def download_image(image_url, name):
-
     # grabs the code from the end of the imgur link, minus the file ext if it exists
     imageID = image_url.split("/")[len(image_url.split("/"))-1].split("\n")[0].split(".")[0] 
     url = "http://api.imgur.com/2/image/" + imageID +  ".json"
@@ -30,12 +30,15 @@ def download_image(image_url, name):
         f.write(r.content)
         os.rename(f.name, gender + "-" + name + "-" + imageID)
 
-for submission in subreddit.get_hot(limit=1000):
-    if ("imgur" in submission.url) and ("/a/" not in submission.url) and ("/" not in submission.title):
+def is_valid_url(url):
+    return ("imgur" in submission.url) and ("/a/" not in submission.url) and ("/" not in submission.title)
+
+for submission in subreddit.get_hot(limit=amount):
+    if is_valid_url(submission.url):
         download_image(submission.url, submission.title)
 
-for submission in subreddit.get_top(limit=1000):
-    if ("imgur" in submission.url) and ("/a/" not in submission.url) and ("/" not in submission.title):
+for submission in subreddit.get_top(limit=amount):
+    if is_valid_url(submission.url):
         download_image(submission.url, submission.title)
 
 
